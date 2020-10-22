0dcef6045e005e601fad0fb4cb27d35ed4ee9a17
zxf <zxf_hb@163.com>
2020-10-22 18:32:48 +0800

    提交变更信息

--- SocketDemo/SocketClient/Program.cs
+++ SocketDemo/SocketClient/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,36 +17,85 @@ namespace SocketClientDemo
         {
             string serverIp = ConfigurationManager.AppSettings.Get("Ip");
             string listenPoint = ConfigurationManager.AppSettings.Get("Point");
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress iP = IPAddress.Parse(serverIp);
             IPEndPoint point = new IPEndPoint(iP, int.Parse(listenPoint));
-            serverSocket.Connect(point);
-            Console.WriteLine("已连接主机：" + serverIp);
+            #region 第一中方式
+            //Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            ////serverSocket.Connect(point);
+            //Console.WriteLine("已连接主机：" + serverIp);
+            //while (true)
+            //{
+            //    Console.WriteLine("请输入需要发送的信息：");
+            //    string sendMessage = Console.ReadLine();
+            //    byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(sendMessage);
+            //    serverSocket.Send(byteArray, SocketFlags.None);
+            //    byte[] by = new byte[100];
+            //    int i = serverSocket.Receive(by, SocketFlags.None);
+            //    if (i < 0)
+            //    {
+            //        Console.WriteLine("已与服务端断开连接，是否尝试重新连接(Y/N)?");
+            //        string result = Console.ReadLine();
+            //        if (result.Equals("Y"))
+            //        {
+            //            serverSocket.Connect(point);
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //    string message = Encoding.UTF8.GetString(by);
+            //    Console.WriteLine( message); 
+            //}
+            //serverSocket.Close();
+            #endregion
+            #region 第二种方式
+
+            Console.WriteLine("Client Running ...");
+            TcpClient client = new TcpClient();
+            try
+            {
+                client.Connect(point);      // 与服务器连接
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //return;
+            }
+            // 打印连接到的服务端信息
+            Console.WriteLine("Server Connected！{0} --> {1}",
+                client.Client.LocalEndPoint, client.Client.RemoteEndPoint);
+
             while (true)
             {
                 Console.WriteLine("请输入需要发送的信息：");
                 string sendMessage = Console.ReadLine();
                 byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(sendMessage);
-                serverSocket.Send(byteArray, SocketFlags.None);
-                byte[] by = new byte[1024];
-                int i = serverSocket.Receive(by, SocketFlags.None);
-                if (i < 0)
+                NetworkStream networkStream = client.GetStream();
+                try
                 {
-                    Console.WriteLine("已与服务端断开连接，是否尝试重新连接(Y/N)?");
-                    string result = Console.ReadLine();
-                    if (result.Equals("Y"))
+                    networkStream.Write(byteArray, 0, byteArray.Length);
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("是否进行重试？（Y/N）");
+                    ConsoleKeyInfo consoleKey = Console.ReadKey(false);
+                    if (consoleKey.Key == ConsoleKey.Y|| consoleKey.Key ==ConsoleKey.Y)
                     {
-                        serverSocket.Connect(point);
+                        continue;
                     }
                     else
                     {
                         break;
                     }
+
                 }
-                string message = Encoding.UTF8.GetString(by);
-                Console.WriteLine("服务器返回结果：" + message);
             }
-            serverSocket.Close();
+            client.Dispose();
+            Console.WriteLine("已退出对话！");
+            Console.ReadLine();
+            #endregion
         }
     }
 }
--- SocketDemo/SocketServer/Program.cs
+++ SocketDemo/SocketServer/Program.cs
@@ -14,48 +14,69 @@ namespace SocketServerDemo
     {
         static void Main(string[] args)
         {
+            byte[] byteCache = new byte[1024];
             string serverIp = ConfigurationManager.AppSettings.Get("Ip");
             string listenPoint = ConfigurationManager.AppSettings.Get("Point");
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress iP = IPAddress.Parse(serverIp);
+            IPAddress iP = IPAddress.Parse(serverIp);//转换为IP地址格式
             IPEndPoint point = new IPEndPoint(iP, int.Parse(listenPoint));
-            serverSocket.Bind(point);
-            Console.WriteLine("服务已启动！");
-            //serverSocket.Connect(point);
-            serverSocket.Listen(2);    //开始监听
-            Console.WriteLine("正在监听端口：" + listenPoint);
-            int index = 1;
-            Socket temp = serverSocket.Accept();//为新建连接创建新的socket
-            while (true)
+            #region 第一种开启服务监听方式
+            //Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //serverSocket.Bind(point);
+            ////Console.WriteLine("服务已启动！");
+            ////serverSocket.Connect(point);
+            //serverSocket.Listen(2);    //开始监听
+            ////Console.WriteLine("正在监听端口：" + listenPoint);
+            //Socket temp = serverSocket.Accept();//为新建连接创建新的socket
+            //while (true)
+            //{
+            //    //接受到client连接，为此连接建立新的socket，并接受信息
+            //    //Console.WriteLine("已与客户端建立连接，等待消息传输！");
+            //    string recvStr = "";
+            //    byte[] recvBytes = new byte[1024];
+            //    int bytes;
+
+            //    try
+            //    {
+            //        bytes = temp.Receive(recvBytes, recvBytes.Length, 0);//从客户端接受信息
+            //    }
+            //    catch (System.Net.Sockets.SocketException ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //        temp = serverSocket.Accept();
+            //        continue;
+            //    }
+            //    if (bytes < 0)
+            //    {
+            //        Console.WriteLine("与客户端连接已断开！");
+            //    }
+            //    recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
+            //    //Console.WriteLine("接收到客户端消息：" + recvStr);
+            //    //Thread.Sleep(1000);
+            //    recvStr = "服务端响应：" + recvStr.ToUpper();
+            //    recvBytes = Encoding.UTF8.GetBytes(recvStr);
+            //    temp.Send(recvBytes, SocketFlags.None);
+            //} 
+
+            #endregion
+
+            #region 第二种开启服务监听方式
+
+            TcpListener tcpListener = new TcpListener(point);
+            tcpListener.Start(10);
+            Console.WriteLine("Start Listening ...");
+            TcpClient tcpClient = tcpListener.AcceptTcpClient();
+            do
             {
-                //接受到client连接，为此连接建立新的socket，并接受信息
-                Console.WriteLine("已与客户端建立连接，等待消息传输！");
-                string recvStr = "";
-                byte[] recvBytes = new byte[1024];
-                int bytes;
+                NetworkStream networkStream = tcpClient.GetStream();
+                int size = networkStream.Read(byteCache, 0, 2);
+                if (size <= 0)
+                {
+                    Console.WriteLine("读取完成！");
 
-                try
-                {
-                    bytes = temp.Receive(recvBytes, recvBytes.Length, 0);//从客户端接受信息
                 }
-                catch (System.Net.Sockets.SocketException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    temp = serverSocket.Accept();
-                    continue;
-                }
-                if (bytes < 0)
-                {
-                    Console.WriteLine("与客户端连接已断开！");
-                }
-                recvStr += Encoding.UTF8.GetString(recvBytes, 0, bytes);
-                Console.WriteLine("接收到客户端消息：" + recvStr);
-                Thread.Sleep(1000);
-                recvStr += "服务端响应序号" + index++;
-                recvBytes = Encoding.UTF8.GetBytes(recvStr);
-                temp.Send(recvBytes, SocketFlags.None);
-            }
-
+            } while (true);
+            #endregion
         }
     }
 }
